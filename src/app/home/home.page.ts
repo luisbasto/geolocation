26efbc67279834ed6870cd77ddfe9c9d1e61b823
Luis Basto <luisbasto@gmail.com>
2021-09-25 14:05:07 -0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  watchId: any;
+  lat: any;
+  lng: any;
+  acc: any;
+  
+  constructor() { }
 
-  constructor() {}
+  ngOnInit() { 
+    this.locate().catch(e => {
+      console.log('There has been a problem with your geolocation: ' + e.message);
+    });
+    this.track();
+  }
+  
+    async locate() {
+      const coordinates = await Geolocation.getCurrentPosition();
+      this.lat = coordinates.coords.latitude;
+      this.lng = coordinates.coords.longitude;
+      this.acc = coordinates.coords.accuracy;
+    }
 
+    async track() {
+      this.watchId = await Geolocation.watchPosition({timeout:2000, enableHighAccuracy:true}, (position, err) => {
+          this.lat = position.coords.latitude;
+          this.lng = position.coords.longitude;
+          this.acc = position.coords.accuracy;
+      })
+    }
 }
